8001cf9205b0b2b1d6ecec8739773c67aebe4382
Léo SAMUEL <leosamuel64@gmail.com>
2020-09-16 19:59:15 +0200

    Devoir pour le 17/09/2020

--- SPE/Chap2_Arbres.ml
+++ SPE/Chap2_Arbres.ml
@@ -69,21 +69,64 @@ let estComplet a =
 
 estComplet exemple;;
 
-let estCompletGauche a=
-  let rec aux a h hInit =
-    (*  
-        a est un arbre
-        h est la hauteur du noeud 
-        hInit est la hauteur de a
-    *)
-    match a with
-    | Noeud(fg,_,fd) -> match 
-    | Vide when h = hInit || h-1=hInit-> true
-    | Vide -> false
-    | Noeud(fg,_,fd) -> (aux fg (h+1) hInit) && (aux fd (h+1) hInit)
-
-  in 
-    aux a 0 (profondeur a)
-;;
 
 (* Pour le 17/09 : Finir l'ex 1 (faire complet gauche a la fin car plus compliqué) *)
+
+let estCompletGauche a =
+  let f = Queue.create () in
+  Queue.add a  f;
+  
+  let rec aux f =
+    if Queue.is_empty f then 
+      true
+    else
+      let t = Queue.take f in
+      match t with
+        | Vide  -> Queue.is_empty f
+        | Noeud(fg,_,fd) -> ( Queue.add fd f ;
+                              Queue.add fg f ; 
+                              aux f )
+  in aux f                 
+;;
+
+let estParfait a=
+  let hinit = profondeur a in
+  let rec aux a h i=
+    match a with
+    | Vide when h = i -> true
+    | Vide -> false
+    | Noeud(fg,_,fd) -> aux fg h (i+1) && aux fd h (i+1)
+  
+  in aux a hinit 0
+;;
+
+
+let rec estEquilibre a=
+  match a with
+      |Vide               -> true
+      | Noeud(fg,_,fd)    -> estEquilibre fg && estEquilibre fd && abs (profondeur fg - profondeur fd) <2
+;;
+
+
+
+let feuille x = Noeud(Vide, x, Vide);;
+let exemple=
+  Noeud (
+     Noeud  (
+         feuille 2,
+         1,
+         feuille 3     
+         ),
+     2,
+     Noeud ( 
+         feuille 1,
+         0,
+         feuille 2
+         )
+     );;
+
+estComplet exemple;;
+estParfait exemple;;
+estCompletGauche exemple;;
+estEquilibre exemple;;
+
